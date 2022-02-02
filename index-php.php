293045d05d05a83ce7361a302030de8ae677325d
ElperdonXV <davidemartinografico@gmail.com>
2022-02-02 15:40:18 +0100

    fixing database

--- index-php.php
+++ index-php.php
@@ -6,11 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 </head>
 <body>
     <!-- Header -->
     <?php include_once __DIR__ . '/partials/header.php' ?>
     <!-- /Header -->
 
+    <!-- Main -->
+    
+    <!-- /Main -->
 </body>
 </html>
